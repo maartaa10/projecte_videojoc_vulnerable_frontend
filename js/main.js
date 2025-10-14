8e784a1441b0aa257c09c2dfa84b752c89fff55b
Marta <marta_rodrigolloveras@iescarlesvallbona.cat>
2025-10-14 17:21:41 +0200

    es crea una primera versio d'en mario i el fons a traves de speed sheets (animacions). A traves de key events es mou el mario

---
+++ js/main.js
@@ -0,0 +1,71 @@
+class Mario {
+    constructor(element) {
+      this.element = element;
+      this.posicio = { x: 50, y: 0 };
+      this.estaSaltant = false;
+      this.alcadaSalt = 100;
+      this.velocitatSalt = 20;
+      this.pas = 10;
+      this.frame = 0;
+  
+      this.actualitzarPosicio();
+    }
+  
+    moure(direccio) {
+      if (direccio === 'esquerra' && this.posicio.x > 0) {
+        this.posicio.x -= this.pas;
+        this.element.style.transform = 'scale(-2, 2)'; // girem horitzontalment
+      } else if (direccio === 'dreta') {
+        this.posicio.x += this.pas;
+        this.element.style.transform = 'scale(2)'; // torna a mirar a la dreta
+      }
+  
+      this.frame = (this.frame + 1) % 3;
+      this.element.style.backgroundPosition = `-${this.frame * 32}px 0px`;
+  
+      this.actualitzarPosicio();
+    }
+  
+    saltar() {
+      if (this.estaSaltant) return;
+  
+      this.estaSaltant = true;
+      let intervalAmunt = setInterval(() => {
+        if (this.posicio.y >= this.alcadaSalt) {
+          clearInterval(intervalAmunt);
+          let intervalAvall = setInterval(() => {
+            if (this.posicio.y <= 0) {
+              clearInterval(intervalAvall);
+              this.posicio.y = 0;
+              this.estaSaltant = false;
+            } else {
+              this.posicio.y -= this.velocitatSalt;
+            }
+            this.actualitzarPosicio();
+          }, 20);
+        } else {
+          this.posicio.y += this.velocitatSalt;
+          this.actualitzarPosicio();
+        }
+      }, 20);
+    }
+  
+    actualitzarPosicio() {
+      this.element.style.left = `${this.posicio.x}px`;
+      this.element.style.bottom = `${this.posicio.y}px`;
+    }
+  }
+  
+  const elementMario = document.querySelector('.mario');
+  const mario = new Mario(elementMario);
+  
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowLeft') {
+      mario.moure('esquerra');
+    } else if (event.key === 'ArrowRight') {
+      mario.moure('dreta');
+    } else if (event.key === 'ArrowUp') {
+      mario.saltar();
+    }
+  });
+  
